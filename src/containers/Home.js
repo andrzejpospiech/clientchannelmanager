a90f11d8d4d44645a73ddfbd61b05bebcbe71b02
EC2 Default User <ec2-user@ip-172-31-41-220.us-east-2.compute.internal>
2018-04-12 16:45:24 +0000

    Initial commit

---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>Channel Manager</h1>
+          <p>Host your properties on affiliated web sites.</p>
+        </div>
+      </div>
+    );
+  }
+}
